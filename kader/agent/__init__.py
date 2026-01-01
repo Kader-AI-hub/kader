262dc0a7d3f78e53b56629b39d7613ef32c2fefd
Ezzaldin97 <kingtal632@yahoo.com>
2026-01-01 02:06:23 -0800

    planning & ReAct agents

--- kader/agent/__init__.py
+++ kader/agent/__init__.py
@@ -3,5 +3,6 @@ Kader Agent module.
 """
 
 from .base import BaseAgent
+from .agents import ReActAgent, PlanningAgent
 
-__all__ = ["BaseAgent"]
+__all__ = ["BaseAgent", "ReActAgent", "PlanningAgent"]
