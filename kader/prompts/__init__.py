4d7b523a1608d1f1be6e5a5ed4cfa6adbe68ed79
Ezzaldin97 <kingtal632@yahoo.com>
2026-01-01 02:05:58 -0800

    add system prompts of planning & ReAct agents

--- kader/prompts/__init__.py
+++ kader/prompts/__init__.py
@@ -1,3 +1,4 @@
 from .base import PromptBase
+from .agent_prompts import BasicAssistancePrompt, ReActAgentPrompt, PlanningAgentPrompt
 
-__all__ = ["PromptBase"]
+__all__ = ["PromptBase", "BasicAssistancePrompt", "ReActAgentPrompt", "PlanningAgentPrompt"]
