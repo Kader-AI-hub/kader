839d0eeee525fd6c7c47a34a138d26723e046fc5
Ezzaldin97 <kingtal632@yahoo.com>
2026-01-29 11:23:02 -0800

    context aggregator for all sub-agents

--- kader/utils/__init__.py
+++ kader/utils/__init__.py
@@ -5,5 +5,6 @@ Provides shared utility functions and helper modules.
 """
 
 from .checkpointer import Checkpointer
+from .context_aggregator import ContextAggregator
 
-__all__ = ["Checkpointer"]
+__all__ = ["Checkpointer", "ContextAggregator"]
