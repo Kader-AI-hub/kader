2cd0a69f29d0ef86cc406b5dcfc8ccf527bc97e4
Ezzaldin97 <kingtal632@yahoo.com>
2026-02-03 12:52:37 -0800

    add google genai provider

--- kader/providers/__init__.py
+++ kader/providers/__init__.py
@@ -1,9 +1,11 @@
 from .base import Message
+from .google import GoogleProvider
 from .mock import MockLLM
 from .ollama import OllamaProvider
 
 __all__ = [
     "Message",
     "OllamaProvider",
+    "GoogleProvider",
     "MockLLM",
 ]
